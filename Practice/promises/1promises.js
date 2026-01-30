af22f0330150340d672723ec8159dd5c4c586375
Sonu Kumar <sonucse103@gmail.com>
2026-01-30 10:33:43 +0530

    create promises file and done some programming

---
+++ Practice/promises/1promises.js
@@ -0,0 +1,66 @@
+// const promisOne = new Promise((resolve, reject) => {
+
+//   setTimeout(() => {
+//     console.log("Async task is complete");
+//      resolve();
+//   }, 1000);
+ 
+// });
+// promisOne.then(()=>{
+//     console.log("Promise consumed")
+// })
+
+// second approch 
+
+// new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//       console.log("promis two resolved")
+//       resolve();
+//     },2000)
+// }).then(()=>{
+//     console.log("promise is consumed");
+// })
+
+// const promise3 = new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         resolve({username:"sonu",email:"sonucse103@gmail.com"})
+//         // resolve("resolved")
+//     },2000)
+// })
+// promise3.then((username)=>{
+//    console.log(username)
+// })
+
+// const promise4 = new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         let eroor = false;
+//         if(!eroor){
+//             resolve({username:"sonu",email:"sonu@gmail.com"})
+//         }else{
+//             reject("Something went wrong")
+//         }
+//     },1000)
+// })
+// promise4.then((user)=>{
+//     return user.username;
+// })
+// .then((username)=>{
+//     console.log(username)
+// }).catch((error)=>{
+//     console.log(error)
+// }).finally(()=>{
+//     console.log("promise is wether fulfill or rejected")
+// });
+
+
+const promise5 = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        let error = true;
+        if(!error){
+            resolve({username:"sonu",email:"sonu@gmail.com"})
+        }else{
+            reject('Error: something went wrong')
+        }
+    },1000)
+
+});
---
+++ Practice/promises/2promise.js
@@ -0,0 +1,15 @@
+// promise race (the promise who is resolved first is returned)
+
+const promise1 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 500, "one");
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 100, "two");
+});
+
+Promise.race([promise1, promise2]).then((value) => {
+  console.log(value);
+  // Both resolve, but promise2 is faster
+});
+// Expected output: "two"
