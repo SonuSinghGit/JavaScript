6e4274aafef4900bdf3f25be23fb3f9dcdb4d28a
Sonu Kumar <sonucse103@gmail.com>
2026-02-01 00:47:03 +0530

    Creata a repository for oops

---
+++ Practice/classesandobjects/1opps.js
@@ -0,0 +1,7 @@
+// objects literals
+const user ={
+    username:"hitesh",
+    logedincount:8,
+    signedin: true
+}
+console.log(user.username)
--- Practice/promises/1promises.js
+++ Practice/promises/1promises.js
@@ -87,13 +87,14 @@
 // getAllusers();
 
 // fetched api througt then and catch
-fetch("https://jsonplaceholder.typicode.com/users")
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+// fetch("https://jsonplaceholder.typicode.com/users")
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
