43caf830bfb8aae29a71f69e4167a127f6207a06
Sonu Kumar <sonucse103@gmail.com>
2026-02-01 22:36:19 +0530

    Start Learning about oops

--- Practice/classesandobjects/1opps.js
+++ Practice/classesandobjects/1opps.js
@@ -1,7 +1,23 @@
 // objects literals
-const user ={
-    username:"hitesh",
-    logedincount:8,
-    signedin: true
+// const user ={
+//     username:"hitesh",
+//     logedincount:8,
+//     signedin: true,
+
+//     getuseDetails: function(){
+//         // console.log("got user from db")
+//         // console.log(`Username: ${this.username}`)
+//         console.log(this) // read more about this keywords
+//     }
+// }
+// console.log(user.username)
+// console.log(user.getuseDetails())
+// console.log(user)
+
+//Important of new keywords(read more about this)
+
+function User(username,loginCount,isLogedIn){
+    this.username= username;
+    this.loginCount = loginCount;
+    this.isLogedIn = isLogedIn;
 }
-console.log(user.username)
