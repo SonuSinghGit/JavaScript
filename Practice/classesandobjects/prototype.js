e768ea763fd1ab184cff1193dd16cc34f336639f
Sonu Kumar <sonucse103@gmail.com>
2026-02-02 23:16:36 +0530

    Add some code

--- Practice/classesandobjects/prototype.js
+++ Practice/classesandobjects/prototype.js
@@ -1,1 +1,16 @@
-let myName= "sonu"
+// let myName= "sonu        "
+// let title = "  singh   "
+
+// console.log(myName.trueLength)
+
+let myhero = ["thor","spiderman"];
+
+let heroPower = {
+    thor:"hammer",
+    spiderman:"sling",
+
+
+    getSpiderPower: function(){
+        console.log(`spiderpower is: ${this.spiderman}`);
+    }
+}
